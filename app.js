84c83b14a61ca3ebadd0679e50a0f6aeac97df81
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-07-18 18:03:45 +0200

    add Middleware in app.js

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const compression = require('compression');
 const mongoose = require('mongoose');
 const logger = require('morgan');
-const statusCode = require('./utilities/statusCode');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const hpp = require('hpp');
 require('dotenv').config();
+
+const statusCode = require('./utilities/statusCode');
 
 const app = express();
 
@@ -14,6 +21,46 @@ app.use(logger('dev'));
 // eslint-disable-next-line no-undef
 const { PORT = 8080, MONGO_URL = '' } = process.env;
 
+// Middleware
+app.use(helmet());
+
+app.use(cors());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false, limit: '30mb' }));
+// parse application/json
+app.use(bodyParser.json({ limit: '30mb' }));
+
+// Make sure the body is parsed beforehand.
+app.use(hpp());
+
+// Compress all HTTP responses
+app.use(
+	compression({
+		filter: (request, response) => {
+			if (request.headers['x-no-compression']) {
+				// don't compress responses with this request header
+				return false;
+			}
+			// fallback to standard filter function
+			return compression.filter(request, response);
+		},
+	})
+);
+
+app.use(
+	rateLimit({
+		windowMs: 60 * 60 * 1000, // 60 minutes
+		max: 100, // Limit each IP to 100 requests per `window` (here, per 60 minutes)
+		message:
+			'Too many accounts created from this IP, please try again after an hour',
+		handler: (_, response, __, options) =>
+			response.status(options.statusCode).json({ error: options.message }),
+		standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+		legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+	})
+);
+
 // Connect To DB
 mongoose
 	.connect(MONGO_URL)
@@ -24,6 +71,9 @@ mongoose
 
 		app.listen(PORT, () => {
 			console.log(`Lama Server listening on port:${PORT}`);
+			console.info(
+				`See: http://localhost:8080/api/health-check To Check Api Status`
+			);
 		});
 	})
 	.catch((error) => {
