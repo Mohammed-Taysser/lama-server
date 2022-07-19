72491f58fd184c16d91465e90b4f40f1fbe4d114
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-07-19 20:25:26 +0200

    create token middleware

---
+++ middleware/auth-token.js
@@ -0,0 +1,23 @@
+const statusCode = require('../utilities/statusCode');
+const { verifyToken } = require('../utilities/jwt');
+
+module.exports = async (request, response, next) => {
+	'use strict';
+	const { token } = request.body;
+	if (token) {
+		await verifyToken(token, (error, user) => {
+			if (error) {
+				response
+					.status(statusCode.error.forbidden)
+					.json({ error: `you aren't authorize, token is invalid` });
+			} else {
+				request.user = user;
+				next();
+			}
+		});
+	} else {
+		response
+			.status(statusCode.error.unauthorized)
+			.json({ error: 'no token provide' });
+	}
+};
