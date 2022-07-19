9f1a7cbef4dc71349de294e32b75249bce5af70d
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-07-19 19:59:08 +0200

    create jwt function (`generateToken` - `verifyToken`)

---
+++ utilities/jwt.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+// eslint-disable-next-line no-undef
+const { JWT_SECRET_KEY = '', JWT_LIFE_TIME } = process.env;
+
+function generateToken(author) {
+	'use strict';
+	return jwt.sign(
+		{
+			_id: author._id,
+			email: author.email,
+			username: author.username,
+			avatar: author.avatar,
+			role: author.role,
+		},
+		JWT_SECRET_KEY,
+		{
+			expiresIn: JWT_LIFE_TIME,
+		}
+	);
+}
+
+function verifyToken(token, callback) {
+	'use strict';
+	return jwt.verify(token, JWT_SECRET_KEY, callback);
+}
+
+module.exports = { generateToken, verifyToken };
