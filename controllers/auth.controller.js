841704a1cb0a33842f8fd5fa680c0785283cb251
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-07-20 19:31:33 +0200

    add login & register validation in routes instead of controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,8 +2,6 @@ const bcrypt = require('bcryptjs');
 const schema = require('../schema/users.schema');
 const statusCode = require('../utilities/statusCode');
 const hashPassword = require('../utilities/hashPassword');
-const loginValidation = require('../validations/login.validate');
-const registerValidation = require('../validations/register.validate');
 const { generateToken } = require('../utilities/jwt');
 
 /**
@@ -12,24 +10,18 @@ const { generateToken } = require('../utilities/jwt');
  * @method POST
  */
 exports.register = async (request, response) => {
-	const errorAsObject = await registerValidation(request.body);
-
-	if (Object.keys(errorAsObject).length === 0) {
-		try {
-			const hashedPassword = await hashPassword(request.body.password);
-			const newUser = new schema({
-				username: request.body.username,
-				email: request.body.email,
-				password: hashedPassword,
-			});
-			const user = await newUser.save();
-			const token = await generateToken(user);
-			response.status(statusCode.success.created).json({ user, token });
-		} catch (error) {
-			response.status(statusCode.error.serverError).json({ error });
-		}
-	} else {
-		response.status(statusCode.error.badRequest).json({ error: errorAsObject });
+	try {
+		const hashedPassword = await hashPassword(request.body.password);
+		const newUser = new schema({
+			username: request.body.username,
+			email: request.body.email,
+			password: hashedPassword,
+		});
+		const user = await newUser.save();
+		const token = await generateToken(user);
+		response.status(statusCode.success.created).json({ user, token });
+	} catch (error) {
+		response.status(statusCode.error.serverError).json({ error });
 	}
 };
 
@@ -41,40 +33,32 @@ exports.register = async (request, response) => {
 exports.login = async (request, response) => {
 	const { email, password } = request.body;
 
-	const errorAsObject = loginValidation(email, password);
-
-	if (Object.keys(errorAsObject).length === 0) {
-		try {
-			// user not exist
-			const user = await schema.findOne({ email });
-			if (!user) {
-				return response.status(statusCode.error.serverError).json({
-					error: {
-						notExist: 'user not exist',
-					},
-				});
-			}
-			// password is bad
-			const isMatch = await bcrypt.compare(password, user.password);
-			if (!isMatch) {
-				return response.status(statusCode.error.serverError).json({
-					error: {
-						password: 'Password Not Correct',
-					},
-				});
-			}
-			// every thing ok
-			const token = await generateToken(user);
-			response.status(statusCode.success.ok).json({
-				user,
-				token,
+	try {
+		// user not exist
+		const user = await schema.findOne({ email });
+		if (!user) {
+			return response.status(statusCode.error.serverError).json({
+				error: {
+					notExist: 'user not exist',
+				},
 			});
-		} catch (error) {
-			response.status(statusCode.error.serverError).json({ error });
 		}
-	} else {
-		response
-			.status(statusCode.error.serverError)
-			.json({ error: errorAsObject });
+		// password is bad
+		const isMatch = await bcrypt.compare(password, user.password);
+		if (!isMatch) {
+			return response.status(statusCode.error.serverError).json({
+				error: {
+					password: 'Password Not Correct',
+				},
+			});
+		}
+		// every thing ok
+		const token = await generateToken(user);
+		response.status(statusCode.success.ok).json({
+			user,
+			token,
+		});
+	} catch (error) {
+		response.status(statusCode.error.serverError).json({ error });
 	}
 };
